f69cbb1702b5858ef491ab262cf5f4273afaa6b4
簑田康平 (minoda kohei) <66286411+Kohei-kun-no@users.noreply.github.com>
2022-01-27 22:02:46 +0900

    complete-button

--- src/index.js
+++ src/index.js
@@ -16,12 +16,30 @@ const onClicked = () => {
 
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
-  completeButton.addEventListener("click", () => {});
+  completeButton.addEventListener("click", () => {
+    deleteFromIncompleteList(completeButton.parentNode);
+
+    const addTarget = completeButton.parentNode;
+    const text = addTarget.firstElementChild.innerText;
+
+    addTarget.textContent = null;
+
+    const li = document.createElement("li");
+    li.innerText = text;
+
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+
+    addTarget.appendChild(li);
+    addTarget.appendChild(backButton);
+
+    document.getElementById("complete-list").appendChild(addTarget);
+  });
 
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
-    const deleteTarget = deleteButton.parentNode;
+    deleteFromIncompleteList(deleteButton.parentNode);
   });
 
   div.appendChild(li);
@@ -32,3 +50,7 @@ const onClicked = () => {
 document
   .getElementById("add-button")
   .addEventListener("click", () => onClicked());
+
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
