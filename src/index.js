b4e88e5c293c40c629cde30ec20c59c3ba843b62
簑田康平 (minoda kohei) <66286411+Kohei-kun-no@users.noreply.github.com>
2022-01-27 21:04:24 +0900

    JSのいろんな確認

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const onClicked = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  div.appendChild(li);
+
+  document.getElementById("incomplete-list").appendChild(div);
+
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {});
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    const deleteTarget = deleteButton.parentNode;
+  });
+
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClicked());
